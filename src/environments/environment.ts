adf83bb059cdc8d15c93db1247483dcd0cfcb429
sirinerh03 <sirine.rhaiem@esprit.tn>
2026-09-17 15:01:29 +0100

    fix: production API URLL

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,5 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://benjeddouerp-backend.onrender.com'
+  apiUrl: 'https://benjeddouerp-backend.onrender.com',
+  backendUrl: 'https://benjeddouerp-backend.onrender.com'
 };
